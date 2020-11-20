e9429b32570f212dfe9c5be27813b1ded439f160
hritu567 <hritumog96@gmail.com>
2020-11-21 02:35:26 +0530

    Added : checking the option whether it is noplay ladder or snake

--- snakeladder.js
+++ snakeladder.js
@@ -1,5 +1,3 @@
 console.log("Welcome to snake and ladder Program");
-var currentPosition = 0;
-console.log("Current position of player : " + currentPosition);
 var Utility= require('./utility');
-Utility.rollDice();
+Utility.checkForOption();
--- utility.js
+++ utility.js
@@ -1,8 +1,33 @@
-//uc2
+//uc3
 // using random function to get the numbers between 1 to 6
-function rollDice()
+var currentPosition = 0;
+var snake=1;
+var ladder=2;
+var noPlay=0;
+console.log("Current position of player : " + currentPosition);
+function checkForOption()
 {
     var ran=Math.floor((Math.random() * 6) + 1);
-    console.log( "The random number we get after rolling the dice : " + ran);
+    console.log("the value appeared after rolling the dice is : " + ran );
+    var check=Math.floor((Math.random() * 10) % 3);
+    console.log("the option is : " + check);
+    switch(check)
+    {
+        case noPlay:
+            currentPosition = 0;
+            console.log("player's current position is : " + currentPosition);
+            break;
+        case snake:
+            currentPosition -= ran;
+            console.log("player's current position is : " + currentPosition);
+            break;
+        case ladder:
+            currentPosition += ran; 
+            console.log("player's current position is : " + currentPosition); 
+            break;
+        default:
+            console.log("nothing has changed");
+            break;
+    }
 }
-module.exports = { rollDice };
+module.exports = { checkForOption };
