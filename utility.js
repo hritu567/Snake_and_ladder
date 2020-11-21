1e4f951918fa17806e93c40a18be7205c18e59e0
hritu567 <hritumog96@gmail.com>
2020-11-21 11:58:45 +0530

    Added : iterating till exact player winning is 100

--- utility.js
+++ utility.js
@@ -1,7 +1,7 @@
 //uc4
 // Repeat till player reaches the wining position 100
 var playerPosition=0;
- var result;
+var result;
 function snake(result)
 {
    playerPosition=playerPosition-result;
@@ -12,37 +12,44 @@ function ladder(result)
     playerPosition=playerPosition+result;
     return playerPosition;
 }
-var currentposition=0;
+var currentPosition=0;
+var previourPosition=0;
 var noplay=0;
 var snakePosition=1;
 var ladderPosition=2;
-while(playerPosition<=100)
-{
-    result=Math.floor(Math.random()*10)%6+1;
+while(playerPosition != 100)
+{   
+    previourPosition = playerPosition;
+    console.log("Dice is rolling.....");
+    result = Math.floor(Math.random()*10)%6+1;
     console.log("Dice rolling result is " + result);
-    var check=Math.floor(Math.random()*10)%3;
+    var check = Math.floor(Math.random()*10)%3;
     // checking snake ladder or noplay;
     switch(check)
     {
       case noplay:
-          currentposition=playerPosition;
-          console.log("No play and player position is : " + currentposition );
+          currentPosition = playerPosition;
+          console.log("No play and player position is : " + currentPosition );
           break;
        case snakePosition:
-           currentposition=snake(result);
+           currentPosition = snake(result);
            console.log("Found Snake");
-           console.log("updated player position : " + currentposition);
+           console.log("updated player position : " + currentPosition);
            break;
        case ladderPosition:
-           currentposition=ladder(result);
+           currentPosition = ladder(result);
            console.log("Found ladder");
-           console.log("updated player position "+currentposition);
+           console.log("updated player position "+ currentPosition);
            break;
            default:
     }
-    if(playerPosition<0)
+    if(playerPosition < 0)
     {
-        playerPosition=0;
+        playerPosition = 0;
+    }
+    if(playerPosition > 100)
+    {
+        playerPosition = previourPosition;
     }
 }
 module.exports = { snake, ladder };
